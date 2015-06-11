10726e9162af54f1a8261607cb000b9e043a032a
El-Terrible <ismoilov92@gmail.com>
2015-06-12 01:05:47 +0700

    добавленно категории меню

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :get_kategory
+
+  private
+  def get_kategory
+    @frute = Prod.find_by_name('Фрукты').id
+    @frut_subcategory = Sprod.all.where(prod_id:[@frute])
+
+    @sfrute = Prod.find_by_name('Сухофрукты').id
+    @sfrut_subcategory = Sprod.all.where(prod_id:[@sfrute])
+
+    @ovoshi = Prod.find_by_name('Овощы').id
+    @ovoshi_subcategory = Sprod.all.where(prod_id:[@ovoshi])
+  end
 end
