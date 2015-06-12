3bfeddfa59e4d8c22180c66a90511efeabd2b6c2
El-Terrible <ismoilov92@gmail.com>
2015-06-12 19:24:44 +0700

    add country and city model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612104745) do
+ActiveRecord::Schema.define(version: 20150612121952) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "country_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "mevas", force: :cascade do |t|
     t.string   "name"
